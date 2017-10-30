d469f6b7f7a407cca96c9da1c8fc70d8f71733f6
Jaume Dellunde <jdellund@xtec.cat>
2017-10-30 09:17:48 +0100

    Conversió pt a dp

--- app/src/main/java/com/edumet/observacions/Observacions_fetes.java
+++ app/src/main/java/com/edumet/observacions/Observacions_fetes.java
@@ -1,5 +1,6 @@
 package com.edumet.observacions;
 
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.BitmapFactory;
@@ -95,13 +96,6 @@ public class Observacions_fetes extends Fragment {
         SimpleDateFormat dateCatala = new SimpleDateFormat("dd-MM-yyyy");
         SimpleDateFormat horaCatala = new SimpleDateFormat("HH:mm");
 
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        params.setMargins(5, 0, 0, 0);
-        LinearLayout.LayoutParams paramsChk = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        paramsChk.setMargins(0, 0, 20, 0);
-        LinearLayout.LayoutParams paramsIcona = new LinearLayout.LayoutParams(200, 200);
-        paramsIcona.setMargins(20, 10, 20, 10);
-
         final LinearLayout lm = (LinearLayout) v.findViewById(R.id.linearLY);
 
         for(int j=0;j<itemDies.size();j++)
@@ -119,6 +113,8 @@ public class Observacions_fetes extends Fragment {
             });
             final ImageView img =new ImageView(getContext());
             img.setId(j+1);
+            LinearLayout.LayoutParams paramsIcona = new LinearLayout.LayoutParams(dpToPx(60), dpToPx(60));
+            paramsIcona.setMargins(dpToPx(10), dpToPx(10), 0, dpToPx(10));
             img.setLayoutParams(paramsIcona);
             img.setImageBitmap(BitmapFactory.decodeFile(itemPath_icons.get(j).toString()));
 
@@ -132,6 +128,11 @@ public class Observacions_fetes extends Fragment {
             } catch (ParseException e) {
                 e.printStackTrace();
             }
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(dpToPx(10), 0, 0, 0);
+
+            lblDia.setLayoutParams(layoutParams);
             ll.addView(lblDia);
 
             TextView lblHora = new TextView(getContext());
@@ -142,31 +143,36 @@ public class Observacions_fetes extends Fragment {
             } catch (ParseException e) {
                 e.printStackTrace();
             }
-            lblHora.setPadding(30,0,40,0);
+
+            lblHora.setLayoutParams(layoutParams);
             ll.addView(lblHora);
 
-             TextView lblFenomen = new TextView(getContext());
-             lblFenomen.setText(nomFenomen(Integer.parseInt(itemFenomens.get(j).toString())));
-            lblFenomen.setLayoutParams(params);
+            TextView lblFenomen = new TextView(getContext());
+            lblFenomen.setText(nomFenomen(Integer.parseInt(itemFenomens.get(j).toString())));
+            lblFenomen.setLayoutParams(layoutParams);
             ll.addView(lblFenomen);
 
             LinearLayout llCheck = new LinearLayout(getContext());
             llCheck.setOrientation(LinearLayout.HORIZONTAL);
-            llCheck.setLayoutParams(paramsChk);
+            LinearLayout.LayoutParams paramsChk = new LinearLayout.LayoutParams(dpToPx(24), dpToPx(24));
+            paramsChk.setMargins(dpToPx(10), dpToPx(10), dpToPx(10), dpToPx(10));
+            LinearLayout.LayoutParams paramsLLChk = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            paramsLLChk.setMargins(0, 0, 0, 0);
+            llCheck.setLayoutParams(paramsLLChk);
             llCheck.setVerticalGravity(Gravity.CENTER_VERTICAL);
             llCheck.setHorizontalGravity(Gravity.RIGHT);
 
             ImageView chk=new ImageView(getContext());
             chk.setImageResource(R.drawable.checkbox_off_background);
-            chk.setPadding(0,0,20,0);
+            chk.setLayoutParams(paramsChk);
             llCheck.addView(chk);
 
             ll.addView(llCheck);
 
             lm.addView(ll);
 
-            RelativeLayout.LayoutParams lineparams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, 1);
             View line = new View(getContext());
+            RelativeLayout.LayoutParams lineparams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, 1);
             line.setLayoutParams(lineparams);
             line.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
 
@@ -196,4 +202,15 @@ public class Observacions_fetes extends Fragment {
                 return "Genèric";
         }
     }
+    
+    public static int dpToPx(int dp)
+    {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
+    public static int pxToDp(int px)
+    {
+        return (int) (px / Resources.getSystem().getDisplayMetrics().density);
+    }
+    
 }
