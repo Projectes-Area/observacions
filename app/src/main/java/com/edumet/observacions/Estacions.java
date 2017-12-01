bf25a7a50377df80486c6749ab1f41b06987cea7
Jaume Dellunde <jdellund@xtec.cat>
2017-12-01 14:54:50 +0100

    Canviat localització dispositiu a Activitat Estacions

--- app/src/main/java/com/edumet/observacions/Estacions.java
+++ app/src/main/java/com/edumet/observacions/Estacions.java
@@ -1,21 +1,41 @@
 package com.edumet.observacions;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,8 +43,26 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class Estacions extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 30000;
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+    private final static String KEY_REQUESTING_LOCATION_UPDATES = "requesting-location-updates";
+    private final static String KEY_LOCATION = "location";
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private SettingsClient mSettingsClient;
+    private LocationRequest mLocationRequest;
+    private LocationSettingsRequest mLocationSettingsRequest;
+    private LocationCallback mLocationCallback;
+    private Location mCurrentLocation;
+    private static boolean mRequestingLocationUpdates;
 
     private GoogleMap mMap;
     private SQLiteDatabase db;
@@ -32,6 +70,8 @@ public class Estacions extends AppCompatActivity implements OnMapReadyCallback {
     DataHelper mDbHelper;
     SharedPreferences sharedPref;
     BottomNavigationView navigation;
+
+    private boolean flagLocalitzada = false;
 
     private int estacioPreferida;
 
@@ -47,17 +87,24 @@ public class Estacions extends AppCompatActivity implements OnMapReadyCallback {
 
         FragmentInfoEstacio secFragment = new FragmentInfoEstacio();
         getSupportFragmentManager().beginTransaction().add(R.id.fragment_info_container, secFragment).commit();
-
+/*
         ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setDisplayHomeAsUpEnabled(true);*/
 
         sharedPref = getSharedPreferences("com.edumet.observacions", MODE_PRIVATE);
-        estacioPreferida=sharedPref.getInt("estacio_preferida", 0);
+        estacioPreferida = sharedPref.getInt("estacio_preferida", 0);
+
+        updateValuesFromBundle(savedInstanceState);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mSettingsClient = LocationServices.getSettingsClient(this);
+        createLocationCallback();
+        createLocationRequest();
+        buildLocationSettingsRequest();
 
         navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         BottomNavigationHelper.disableShiftMode(navigation);
-        //navigation.setSelectedItemId(R.id.navigation_estacions);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -68,19 +115,19 @@ public class Estacions extends AppCompatActivity implements OnMapReadyCallback {
             Intent intent;
             switch (item.getItemId()) {
                 case R.id.navigation_observacions:
-                    intent = new Intent(getApplicationContext(),MainActivity.class);
+                    intent = new Intent(getApplicationContext(), MainActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
                     return true;
                 case R.id.navigation_estacions:
                     return true;
                 case R.id.navigation_radar:
-                    intent = new Intent(getApplicationContext(),Radar.class);
+                    intent = new Intent(getApplicationContext(), Radar.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
                     return true;
                 case R.id.navigation_pronostic:
-                    intent = new Intent(getApplicationContext(),Pronostic.class);
+                    intent = new Intent(getApplicationContext(), Pronostic.class);
                     //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
                     return true;
@@ -88,6 +135,147 @@ public class Estacions extends AppCompatActivity implements OnMapReadyCallback {
             return false;
         }
     };
+    //
+    // LOCALITZACIÓ
+    //
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean(KEY_REQUESTING_LOCATION_UPDATES, mRequestingLocationUpdates);
+        savedInstanceState.putParcelable(KEY_LOCATION, mCurrentLocation);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            if (savedInstanceState.keySet().contains(KEY_REQUESTING_LOCATION_UPDATES)) {
+                mRequestingLocationUpdates = savedInstanceState.getBoolean(KEY_REQUESTING_LOCATION_UPDATES);
+            }
+            if (savedInstanceState.keySet().contains(KEY_LOCATION)) {
+                mCurrentLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            }
+            updateLocationUI();
+        }
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        Log.i("..Location Request", "Done");
+    }
+
+    private void createLocationCallback() {
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                mCurrentLocation = locationResult.getLastLocation();
+                updateLocationUI();
+            }
+        };
+        Log.i("..Location Callback", "Done");
+    }
+
+    private void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+        Log.i("..Location Settings", "Done");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i("..onActResult", "Done");
+        switch (requestCode) {
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case RESULT_OK:
+                        Log.i("..OnActResult", "User agreed to make required location settings changes.");
+                        mRequestingLocationUpdates = true;
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.i("..OnActResult", "User chose not to make required location settings changes.");
+                        mRequestingLocationUpdates = false;
+                        break;
+                }
+                updateLocationUI();
+                Log.i("..OnActResult", String.valueOf(mRequestingLocationUpdates));
+                break;
+        }
+    }
+
+    private void startLocationUpdates() {
+        mSettingsClient.checkLocationSettings(mLocationSettingsRequest)
+                .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+                    @SuppressLint("MissingPermission")
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        Log.i("..startLocation", "All location settings are satisfied.");
+                        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+                        mRequestingLocationUpdates = true;
+                        updateLocationUI();
+
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                Log.i("..startUpdates", "Location settings are not satisfied. Attempting to upgrade location settings.");
+                                try {
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(Estacions.this, REQUEST_CHECK_SETTINGS);
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.i("..startUpdates", "PendingIntent unable to execute request.");
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                //Toast.makeText(getActivity(), R.string.fix_settings, Toast.LENGTH_LONG).show();
+                                mRequestingLocationUpdates = false;
+                        }
+                        updateLocationUI();
+                    }
+                });
+    }
+
+    private void stopLocationUpdates() {
+        if (!mRequestingLocationUpdates) {
+            Log.i("..stopUpdates", "stopLocationUpdates: updates never requested, no-op.");
+            return;
+        }
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        mRequestingLocationUpdates = false;
+                    }
+                });
+    }
+
+    private void updateLocationUI() {
+        if (mCurrentLocation != null) {
+            if (!flagLocalitzada) {
+                Snackbar.make(findViewById(android.R.id.content), "S'ha localitzat la teva ubicació", Snackbar.LENGTH_SHORT).show();
+                //((MainActivity) getActivity()).ubicacio(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+                flagLocalitzada = true;
+                //desaPreferencies();
+            }
+        }
+    }
+
+    //
+    // MAPA
+    //
 
     public void obreMapa() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa_estacions);
@@ -104,7 +292,7 @@ public class Estacions extends AppCompatActivity implements OnMapReadyCallback {
                 Database.Estacions.COLUMN_NAME_LONGITUD,
         };
 
-        String selection = Database.Estacions._ID+ " > ?";
+        String selection = Database.Estacions._ID + " > ?";
         String[] selectionArgs = {"0"};
         String sortOrder = null;
 
@@ -118,14 +306,13 @@ public class Estacions extends AppCompatActivity implements OnMapReadyCallback {
             @Override
             public boolean onMarkerClick(Marker marker) {
                 FragmentEstacions fragment = (FragmentEstacions) getSupportFragmentManager().findFragmentById(R.id.fragment_estacions_container);
-                //fragment.buidaInfo();
-                fragment.clicaSpinner(Integer.valueOf(marker.getSnippet())-1);
+                fragment.clicaSpinner(Integer.valueOf(marker.getSnippet()) - 1);
                 return true;
             }
         });
 
-        estacioPreferida=sharedPref.getInt("estacio_preferida", 0);
-        int ID_estacioPreferida=0;
+        estacioPreferida = sharedPref.getInt("estacio_preferida", 0);
+        int ID_estacioPreferida = 0;
 
         LatLng observacio;
 
@@ -136,15 +323,14 @@ public class Estacions extends AppCompatActivity implements OnMapReadyCallback {
                     .position(observacio)
                     .snippet(cursor.getString(cursor.getColumnIndexOrThrow(Database.Estacions._ID)))
             );
-            if(cursor.getInt(cursor.getColumnIndexOrThrow(Database.Estacions.COLUMN_NAME_ID_EDUMET))==estacioPreferida) {
-                ID_estacioPreferida=cursor.getInt(cursor.getColumnIndexOrThrow(Database.Estacions._ID));
+            if (cursor.getInt(cursor.getColumnIndexOrThrow(Database.Estacions.COLUMN_NAME_ID_EDUMET)) == estacioPreferida) {
+                ID_estacioPreferida = cursor.getInt(cursor.getColumnIndexOrThrow(Database.Estacions._ID));
             }
         }
         cursor.close();
 
         FragmentEstacions fragment = (FragmentEstacions) getSupportFragmentManager().findFragmentById(R.id.fragment_estacions_container);
-        //fragment.buidaInfo();
-        fragment.clicaSpinner(ID_estacioPreferida-1);
+        fragment.clicaSpinner(ID_estacioPreferida - 1);
     }
 
     @Override
@@ -177,15 +363,20 @@ public class Estacions extends AppCompatActivity implements OnMapReadyCallback {
     }
 
     @Override
-    public void onBackPressed() {
-        super.onBackPressed();
+    public void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+        Log.i("..OnPause", String.valueOf(mRequestingLocationUpdates));
     }
 
     @Override
     public void onResume() {
         super.onResume();
         navigation.setSelectedItemId(R.id.navigation_estacions);
+        Log.i(".OnResume", String.valueOf(mRequestingLocationUpdates));
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
     }
-
 
 }
